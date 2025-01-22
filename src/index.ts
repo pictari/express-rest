a15a28bf366295cc991abb49f61482215c759ab8
lyra-school <s00233718@atu.ie>
2025-01-22 21:45:23 +0000

    Added TypeORM and related database libraries

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import roomsRouter from "./routes/rooms";
+import "reflect-metadata";
 const cors = require('cors');
 
 const PORT = 3000;
