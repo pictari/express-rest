0c57fb512875c0971017a9b1624f591ba05bd7aa
lyra-school <s00233718@atu.ie>
2025-02-04 23:22:38 +0000

    Added router for existing account endpoints

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import roomsRouter from "./routes/rooms";
+import accountRouter from "./routes/account";
 import "reflect-metadata";
 const cors = require('cors');
 
@@ -15,6 +16,7 @@ app.use(morgan("tiny"));
 app.use(express.json());
 
 app.use('/rooms', roomsRouter);
+app.use('/account', accountRouter);
 
 var server = app.listen(PORT, function () {
     console.log("Listening on port: " + PORT)
---
+++ src/routes/account.ts
@@ -0,0 +1,24 @@
+import express, {Router} from 'express';
+import { getAccountBlockedList, getAccountFriends, getAccountPendingFriendships, getPersonalAccountInfo, getPublicAccountShortened, getPublicAccountStatistics } from '../controllers/account';
+
+const router: Router = express.Router();
+
+router.get('/:uuid', getPublicAccountShortened);
+router.get('/:uuid/profile', getPublicAccountStatistics);
+router.get('/:uuid/profile/settings', getPersonalAccountInfo);
+router.get('/:uuid/friends', getAccountFriends);
+router.get('/:uuid/friends/pending', getAccountPendingFriendships);
+//router.get('/:uuid/friends/:uuid2', endpointForAcceptingRequests);
+router.get('/:uuid/blocks', getAccountBlockedList);
+
+// change to auth route later
+//router.post('/', endpointForNewAccount);
+
+//router.post('/:uuid/friends/:uuid2', endpointForMakingFriends);
+//router.post('/:uuid/blocks/:uuid2', endpointForMakingBlocks);
+
+//router.put('/:uuid/profile/settings', endpointForChangingSettings);
+
+//router.delete('/:uuid/friends/:uuid2', endpointForDeletingFriendship);
+//router.delete('/:uuid/blocks/:uuid2', endpointForDeletingBlocks);
+export default router;
