c1f35348ae4f9044175c574d8aae0da74bd2ab3f
lyra-school <s00233718@atu.ie>
2025-02-05 00:19:38 +0000

    Added object and validators for account-related CRUD

---
+++ src/models/rest/account.ts
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+export interface Account {
+    email?:string,
+    name?:string,
+    about?:string,
+    password?:string
+}
+
+export const ValidateAccountPost = (account: Account) => {
+    const validator = Joi.object<Account>({
+        email: Joi.string().email().required(),
+        name: Joi.string().min(3).max(16).required(),
+        password: Joi.string().required()
+    });
+    validator.validate(account);
+}
+
+export const ValidateAccountPut = (account: Account) => {
+    const validator = Joi.object<Account>({
+        email: Joi.string().email().optional(),
+        name: Joi.string().min(3).max(16).optional(),
+        about: Joi.string().max(255).optional(),
+        password: Joi.string().optional()
+    });
+    validator.validate(account);
+}
