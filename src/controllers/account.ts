b716aaab7b3d89b784eac4eb02eba09905a5551a
lyra-school <s00233718@atu.ie>
2025-02-09 01:24:29 +0000

    Added Create endpoint for blocks

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -5,6 +5,7 @@ import { Verification } from "../models/orm/verification";
 import { Friendship } from "../models/orm/friendship";
 import { Block } from "../models/orm/block";
 import { PendingFriendship } from "../models/orm/pending_friendship";
+import { UUID } from "crypto";
 
 const accountRepo = AccountDataSource.getRepository(Account);
 const verificationRepo = AccountDataSource.getRepository(Verification);
@@ -157,11 +158,14 @@ export const getAccountBlockedList = async (req: Request, res: Response) => {
     }
 }
 
+// self explanatory
 export const postNewFriendRequest  = async (req: Request, res: Response) => {
     let requesterUuid = req.params.uuid;
     let requestedUuid = req.params.uuid2;
 
     // does the requester UUID even exist?
+    // these checks also double in function as fetching the needed accounts to
+    // create a request
     let verifyExistence = await accountRepo.findOneBy({ uuid: requesterUuid})
 
     if(verifyExistence == null) {
@@ -172,6 +176,15 @@ export const postNewFriendRequest  = async (req: Request, res: Response) => {
 
     if(verifyExistence2 == null) {
         res.status(404).send(`The requested UUID doesn't exist.`);
+    }
+
+    // check that the friendship doesn't already exist
+    let toSort = [requesterUuid, requestedUuid];
+    toSort.sort();
+    let friends = await friendshipRepo.findOneBy({ accountUuid:toSort[0], account2Uuid:toSort[1]});
+
+    if(friends != null) {
+        res.status(400).send(`This friendship already exists.`);
     }
 
     // check that the request doesn't exist already
@@ -190,14 +203,69 @@ export const postNewFriendRequest  = async (req: Request, res: Response) => {
     });
 
     if(requests.length != 0) {
-        // 409 = Conflict
-        res.status(409).send(`A request for this friendship already exists.`);
+        res.status(400).send(`A request for this friendship already exists.`);
     }
 
-    const pendingRequest = new PendingFriendship()
+    const pendingRequest = new PendingFriendship();
     pendingRequest.account == verifyExistence;
     pendingRequest.account2 == verifyExistence2;
     
     await AccountDataSource.manager.save(pendingRequest);
     res.status(201).send(`Created a friendship request.`);
+}
+
+// self explanatory
+export const postNewBlock  = async (req: Request, res: Response) => {
+    let requesterUuid = req.params.uuid;
+    let requestedUuid = req.params.uuid2;
+
+    // does the requester UUID even exist?
+    // these checks also double in function as fetching the needed accounts to
+    // create a request
+    let verifyExistence = await accountRepo.findOneBy({ uuid: requesterUuid})
+
+    if(verifyExistence == null) {
+        res.status(404).send(`The requester UUID doesn't exist.`);
+    } 
+    // does the requested UUID even exist?
+    let verifyExistence2 = await accountRepo.findOneBy({ uuid: requestedUuid})
+
+    if(verifyExistence2 == null) {
+        res.status(404).send(`The requested UUID doesn't exist.`);
+    }
+
+
+    // check that block doesn't already exist
+    let blocked = await blockRepo.findOneBy({accountUuid: requesterUuid});
+
+    if(blocked != null) {
+        res.status(400).send(`This account was already blocked.`);
+    }
+
+    deleteFriendship(requesterUuid, requestedUuid);
+
+    const newBlock = new Block();
+    newBlock.account == verifyExistence;
+    newBlock.account2 == verifyExistence2;
+}
+
+async function deleteFriendship(uuid1: string, uuid2: string) {
+        let toSort = [uuid1, uuid2];
+        toSort.sort();
+        let friends = await friendshipRepo.findOneBy({ accountUuid:toSort[0], account2Uuid:toSort[1]});
+    
+        if(friends == null) {
+            return false;
+        }
+
+        await AccountDataSource
+            .createQueryBuilder()
+            .delete()
+            .from(Friendship)
+            .where("accountUuid = :id, account2Uuid = :id2", { id: toSort[0], id2: toSort[1] })
+            .execute().then((value) => {
+                console.log(value);
+            });
+        
+        return true;
 }
--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,5 +1,5 @@
 import express, {Router} from 'express';
-import { getAccountBlockedList, getAccountFriends, getAccountPendingFriendships, getPersonalAccountInfo, getPublicAccountShortened, getPublicAccountStatistics, postNewFriendRequest } from '../controllers/account';
+import { getAccountBlockedList, getAccountFriends, getAccountPendingFriendships, getPersonalAccountInfo, getPublicAccountShortened, getPublicAccountStatistics, postNewBlock, postNewFriendRequest } from '../controllers/account';
 
 const router: Router = express.Router();
 
@@ -14,11 +14,12 @@ router.get('/:uuid/blocks', getAccountBlockedList);
 //router.post('/', endpointForNewAccount);
 
 router.post('/:uuid/friends/:uuid2', postNewFriendRequest);
-//router.post('/:uuid/blocks/:uuid2', endpointForMakingBlocks);
-//router.post('/:uuid/friends/:uuid2/accept', endpointForAcceptingRequests);
+router.post('/:uuid/blocks/:uuid2', postNewBlock);
+//router.post('/:uuid/friends/:uuid2/pending', endpointForAcceptingRequests);
 
 //router.put('/:uuid/profile/settings', endpointForChangingSettings);
 
 //router.delete('/:uuid/friends/:uuid2', endpointForDeletingFriendship);
 //router.delete('/:uuid/blocks/:uuid2', endpointForDeletingBlocks);
+//router.delete('/:uuid/friends/:uuid2/pending', endpointForRejectingRequests);
 export default router;
