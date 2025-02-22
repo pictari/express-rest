22f0ff634229d3d2fb96f3c0f4d1c4bfcd9a71c3
lyra-school <s00233718@atu.ie>
2025-02-22 19:22:52 +0000

    Added JWT verification

---
+++ app/src/auth/auth.ts
@@ -0,0 +1,84 @@
+import { Request, Response, NextFunction } from "express";
+import { verify as jwtVerify } from "jsonwebtoken";
+
+export const verifyJWT = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+
+
+    const authHeader = req.headers?.authorization;
+
+    if (!authHeader || !authHeader?.startsWith('Bearer')) {
+        res.status(401).send("No Bearer auth header found. Make sure that you're inserting the token in an appropriate header.");
+        return;
+    }
+
+    const token: string | undefined = authHeader.split(' ')[1];
+
+    if (!token) {
+        res.status(401).send();
+        return;
+    }
+
+    if(process.env.JWTSECRET == undefined) {
+        res.status(500).send("The server has a misconfigured secret key. Please let the administrators know.");
+        return;
+    }
+
+    const secret = process.env.JWTSECRET;
+
+    try {
+        const payload = jwtVerify(token, secret);
+        res.locals.payload = payload;
+        next();
+    } catch (err) {
+        res.status(403).send();
+        return;
+    }
+};
+
+export const verifyAdministrator = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const jwtRole = res.locals?.payload?.role;
+    if (jwtRole == 0) {
+        next();
+    }
+    else {
+        res.status(403).send("This is a resource scoped to administrators.");
+    }
+};
+
+export const verifyModerator = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const jwtRole = res.locals?.payload?.role;
+    if (jwtRole == 0 || jwtRole == 1) {
+        next();
+    }
+    else {
+        res.status(403).send("This is a resource scoped to moderators and above.");
+    }
+};
+
+export const verifyOwner = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const jwtRole = res.locals?.payload?.role;
+    const requestedId = req.params.uuid;
+    const userId = res.locals?.payload?.uuid;
+    if (jwtRole == 0 || jwtRole == 1 || userId == requestedId) {
+        next();
+    }
+    else {
+        res.status(403).send("This resource is scoped to its owner and moderators.");
+    }
+};
