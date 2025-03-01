a3090e6b0555721d3bcbf534b45ffa1c813e0c18
lyra-school <s00233718@atu.ie>
2025-03-01 22:40:43 +0000

    Added authorization constraints on the endpoints

--- app/src/routes/account.ts
+++ app/src/routes/account.ts
@@ -1,28 +1,32 @@
 import express, {Router} from 'express';
 import { deleteBlock, deleteFriend, deleteRequest, getAccountBlockedList, getAccountFriends, getAccountPendingFriendships, getAccountSearchByName, getPersonalAccountInfo, getPublicAccountShortened, getPublicAccountStatistics, postAcceptRequest, postNewAccount, postNewBlock, postNewFriendRequest, putNewAccountSettings } from '../controllers/account';
+import { verifyJWT, verifyOwner, verifyVerification } from '../auth/auth';
 
 
 const router: Router = express.Router();
 
 router.get('/:uuid', getPublicAccountShortened);
 router.get('/:uuid/profile', getPublicAccountStatistics);
-router.get('/:uuid/profile/settings', getPersonalAccountInfo);
+router.get('/:uuid/profile/settings', verifyJWT, verifyOwner, getPersonalAccountInfo);
 router.get('/:uuid/friends', getAccountFriends);
-router.get('/:uuid/friends/pending', getAccountPendingFriendships);
-router.get('/:uuid/blocks', getAccountBlockedList);
 
-router.get('/search/:name', getAccountSearchByName);
+// it may not be desirable to make these public
+router.get('/:uuid/friends/pending', verifyJWT, verifyOwner, getAccountPendingFriendships);
+router.get('/:uuid/blocks', verifyJWT, verifyOwner, getAccountBlockedList);
 
-// change to auth route later
+// you will only ever need to use this function when you're logged in, so JWT verification prevents stress on the database
+router.get('/search/:name', verifyJWT, getAccountSearchByName);
+
 router.post('/', postNewAccount);
 
-router.post('/:uuid/friends/:uuid2', postNewFriendRequest);
-router.post('/:uuid/blocks/:uuid2', postNewBlock);
-router.post('/:uuid/friends/:uuid2/pending', postAcceptRequest);
+router.post('/:uuid/friends/:uuid2', verifyJWT, verifyVerification, verifyOwner, postNewFriendRequest);
+router.post('/:uuid/blocks/:uuid2', verifyJWT, verifyVerification, verifyOwner, postNewBlock);
+router.post('/:uuid/friends/:uuid2/pending', verifyJWT, verifyVerification, verifyOwner, postAcceptRequest);
 
-router.put('/:uuid/profile/settings', putNewAccountSettings);
+router.put('/:uuid/profile/settings', verifyJWT, verifyOwner, putNewAccountSettings);
 
-router.delete('/:uuid/friends/:uuid2', deleteFriend);
-router.delete('/:uuid/blocks/:uuid2', deleteBlock);
-router.delete('/:uuid/friends/:uuid2/pending', deleteRequest);
+// if non-verified users can't create friends/blocks/etc, reduce stress on database checking account relationships
+router.delete('/:uuid/friends/:uuid2', verifyJWT, verifyVerification,  verifyOwner, deleteFriend);
+router.delete('/:uuid/blocks/:uuid2', verifyJWT, verifyVerification,  verifyOwner, deleteBlock);
+router.delete('/:uuid/friends/:uuid2/pending', verifyJWT, verifyVerification,  verifyOwner, deleteRequest);
 export default router;
--- app/src/routes/rooms.ts
+++ app/src/routes/rooms.ts
@@ -1,10 +1,13 @@
 import express, {Router} from 'express';
 import { getPrivateRoomDetails, getRoomDetails, getRoomsList } from '../controllers/rooms';
+import { verifyJWT, verifyVerification } from '../auth/auth';
 
 const router: Router = express.Router();
 
 router.get('/', getRoomsList);
-router.get('/:id', getRoomDetails);
-router.get('/private/:key', getPrivateRoomDetails);
+
+// only logged-in and verified players can join for now, so this prevents stress on the database
+router.get('/:id', verifyJWT, verifyVerification, getRoomDetails);
+router.get('/private/:key', verifyJWT, verifyVerification, getPrivateRoomDetails);
 
 export default router;
