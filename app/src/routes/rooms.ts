02a9defd2cad06a53ac519659c604c4e7f6741cb
lyra-school <s00233718@atu.ie>
2025-03-09 22:50:53 +0000

    Removed auth from the room details route

--- app/src/routes/rooms.ts
+++ app/src/routes/rooms.ts
@@ -6,8 +6,10 @@ const router: Router = express.Router();
 
 router.get('/', getRoomsList);
 
+
+router.get('/:id', getRoomDetails);
+
 // only logged-in and verified players can join for now, so this prevents stress on the database
-router.get('/:id', verifyJWT, verifyVerification, getRoomDetails);
 router.get('/private/:key', verifyJWT, verifyVerification, getPrivateRoomDetails);
 
 export default router;
